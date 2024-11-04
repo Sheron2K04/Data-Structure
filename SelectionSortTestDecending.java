a6028b6134d078eb0233b5251b95e13fc29e74f7
Sheron2K04 <sheronfernando2004128@gmail.com>
2024-11-04 16:36:33 +0530

    Add files via upload

---
+++ SelectionSortTestDecending.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class SelectionSortTestDecending {
+    Integer[] a;
+
+    public SelectionSortTestDecending(Integer[] a) {
+        this.a = a;
+    }
+
+    public void selectionSort() {
+        int n = a.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int max_idx = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[max_idx]) {
+                    max_idx = j;
+                }
+                System.out.println(
+                    " i = "
+                    + i
+                    + "; j = "
+                    + j
+                    + "; cur_max = "
+                    + a[max_idx]
+                    + "; "
+                    + Arrays.deepToString(a)
+                );
+            }
+            swap(i, max_idx);
+        }
+    }
+
+    private void swap(int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] array = {76, 6, 107, 92, 21, 23, 5, 9, 8, 8143};
+
+        SelectionSortTestDecending sorter = new SelectionSortTestDecending(array);
+        sorter.selectionSort();
+        System.out.println("Array after sorting: " + Arrays.deepToString(sorter.a));
+
+    }
+}
